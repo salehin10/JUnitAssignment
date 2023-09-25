20ca8c75095d3f32c70a865494ac6a16bfcc0fcb
Your Name <sirajuz.salehin@gmail.com>
2023-09-25 23:58:46 +0600

    upload 2

--- src/test/java/MyJunitTesting.java
+++ src/test/java/MyJunitTesting.java
@@ -43,7 +43,7 @@ public class MyJunitTesting{
         driver.findElement(By.className("form-date")).sendKeys("05/25/2023");
         driver.findElement(By.className("form-email")).sendKeys("test@test.com");
         JavascriptExecutor jse = (JavascriptExecutor)driver;
-        jse.executeScript("window.scrollBy(0,350)");
+        jse.executeScript("window.scrollBy(0,500)");
         driver.findElement(By.className("form-textarea")).sendKeys("Lorem Ipsum is simply dummy ## >! ?? of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an deocde?!# $$code. anotehr plot<< >>  printer took a galley of type and scrambled it to make a type specimen book.");
         driver.findElement(By.className("form-file")).sendKeys("D:\\OneDrive - ReliSource Inc\\RTL Onedrive\\OneDrive - ReliSource Inc\\vi-eur\\Video\\SQA Automation\\SDET Batch 7(Job Holder Batch)\\data_sheet.xlsx");
         Thread.sleep(10000);
@@ -55,9 +55,6 @@ public class MyJunitTesting{
         String titleText = driver.findElement(By.id("block-pagetitle-2")).getText();
         System.out.println("Title to next page : "+titleText);
         Assertions.assertEquals(titleText, "Thank you for your submission!");
-
-
-
 
     }
 
